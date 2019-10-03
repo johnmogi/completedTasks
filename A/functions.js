8a2b472af2017c1fed14af27816cfc453afca1bc
johnmogi <anguru@gmail.com>
2019-10-03 21:18:52 +0300

    @rebase@

--- A/functions.js
+++ A/functions.js
@@ -1,48 +1,48 @@
 function clear() {
   stage.innerHTML = ""
-  localStorage.clear();
-  taskArray.length = 0;
-  returnedArray.length = 0;
+   localStorage.clear();
+   taskArray.length = 0;
+   returnedArray.length = 0;
 }
 
-(function () {
-  'use strict';
-  window.addEventListener('load', function () {
+          (function() {
+            'use strict';
+            window.addEventListener('load', function() {
 
-    // window.addEventListener('load', validateBs)  
+            // window.addEventListener('load', validateBs)  
+            
+            // function validateBs(){ 
+              // this.console.log("ready functions")
+              // Fetch all the forms we want to apply custom Bootstrap validation styles to
+              var forms = document.getElementsByClassName('needs-validation');
+              // Loop over them and prevent submission
+              var validation = Array.prototype.filter.call(forms, function(form) {
+                butonSub.addEventListener('click', function(event) {
+                  if (form.checkValidity() === false) {
+                    event.preventDefault();
+                    event.stopPropagation();
+                  }
+                  form.classList.add('was-validated');
+                }, false);
+              });
+            }, false);
+          })();
 
-    // function validateBs(){ 
-    // this.console.log("ready functions")
-    // Fetch all the forms we want to apply custom Bootstrap validation styles to
-    var forms = document.getElementsByClassName('needs-validation');
-    // Loop over them and prevent submission
-    var validation = Array.prototype.filter.call(forms, function (form) {
-      butonSub.addEventListener('click', function (event) {
-        if (form.checkValidity() === false) {
-          event.preventDefault();
-          event.stopPropagation();
-        }
-        form.classList.add('was-validated');
-      }, false);
-    });
-  }, false);
-})();
-
-// }
+        // }
 
 
 
-// assign current date to date:
+          // assign current date to date:
 var today = new Date();
 // var dd = String(today.getDate()).padStart(2, '0');
 var dd = today.getDate()
 // var mm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
-var mm = today.getMonth() + 1
+var mm = today.getMonth()+1
 
 var yyyy = today.getFullYear();
 
 // today = mm + '/' + dd + '/' + yyyy;
-today = [dd, mm, yyyy]
+today = [dd,mm,yyyy]
 
 const taskCreationDate = (today);
 
@@ -50,25 +50,25 @@ const taskCreationDate = (today);
 
 
 
-function timeValid() {
-  let datenow = dueDate.value.split("-")
-  let yearFromInput = parseInt(datenow[0])
-  let dayFromInput = parseInt(datenow[2])
+function timeValid(){
+  let datenow =  dueDate.value.split("-")
+  let  yearFromInput= parseInt(datenow[0])
+  let dayFromInput  = parseInt(datenow[2])
   let monthFromInput = parseInt(datenow[1])
-  if (yearFromInput < yyyy ||
+  if (yearFromInput < yyyy || 
     monthFromInput < mm,
     yearFromInput <= yyyy &&
     monthFromInput < mm,
     yearFromInput <= yyyy &&
     monthFromInput <= mm &&
-    dayFromInput < dd) {
-    //    if (monthFromInput >= mm) { 
-    //  {
-    messageBoard.innerText = ("please choose a future date")
+    dayFromInput < dd) { 
+//    if (monthFromInput >= mm) { 
+//  {
+  messageBoard.innerText = ("please choose a future date")
     messageBoard.style.color = "red"
-    //  }
-  }
-}
+  //  }
+ }
+ }
 
 
 
@@ -84,12 +84,13 @@ function timeValid() {
 //   }
 //   }
 
-function validate() {
-  if (!taskName.value || !taskDetail.value) {
+function validate(){
+   if (!taskName.value || !taskDetail.value ) {
     messageBoard.innerText = ("Please Fill All Inputs")
     messageBoard.style.color = "red"
     // alert("Please Fill All Inputs")
     return;
-  }
-  timeValid()
 }
+   timeValid()
+}
+
